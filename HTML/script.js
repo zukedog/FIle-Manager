2acbf515c99df17a0eb24ee9c2988b3ff83dbfec
Joshua SUTTON <joshua.sutton@student.ecc.sa.edu.au>
2018-05-23 15:04:41 +0930

    modified code

--- HTML/script.js
+++ HTML/script.js
@@ -1,14 +1,48 @@
 // JavaScript Document
+/*
 var zip = new JSZip();
 zip.file("josh","stuffs");
 var jDir = zip.folder("jDir").file("bob","stuffs");
-/*
+
+
 jDir.generateAsync({type:"blob"})
 .then(function (blob) {
     saveAs(blob, "hello.zip");
 });
-*/
+
+
 console.log(zip);
 zip.forEach(function(stuffs){
 	console.log(stuffs);
 });
+*/
+
+$("li").on("click", function(e){
+	$(e).children(":checkbox").first().hide();
+});
+
+
+var ID = 0;
+
+function item(type, name, contents) {
+	this.type = type;
+	this.name = name;
+	this.ID = ID;
+	ID++;
+	if (this.type == "dir") {
+		this.contents = contents || [];
+		this.addItem = function(type, name, contents){
+			this.contents.push(new item(type, name, contents));
+		}
+	} else {
+		this.contents = contents || "";
+	}
+}
+
+
+
+var me = new item("dir");
+me.addItem("dir");
+me.addItem("file");
+console.log(me);
+
