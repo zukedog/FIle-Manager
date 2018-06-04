e6ac32a7294a9fa6f8deed68382f158c5eaf3497
zukedog <zukedog18@gmail.com>
2018-06-04 19:44:56 +0930

    generate tree from object

--- HTML/script.js
+++ HTML/script.js
@@ -17,7 +17,7 @@ zip.forEach(function(stuffs){
 });
 
 */
-$("li").on("click", function(e){
+$("li").on("click", function (e) {
 	$(e).children(":checkbox").first().hide();
 });
 
@@ -31,24 +31,58 @@ function item(type, name, contents) {
 	ID++;
 	if (this.type == "dir") {
 		this.contents = contents || [];
-		this.addItem = function(type, name, contents){
+		this.addItem = function (type, name, contents) {
 			this.contents.push(new item(type, name, contents));
 		}
+		this.getContents = function () {
+			var cont = "";
+
+			for (var i = 0; i < this.contents.length; i++) {
+				cont += this.contents[i].html();
+			}
+			return cont;
+		};
 	} else {
 		this.contents = contents || "";
 	}
+
+	this.html = function () {
+		if (this.type == "dir") {
+			return `<li>
+			<input type='checkbox' name='file' value='none' id="arrow${this.ID}" class="arrow"/><label for="arrow${this.ID}"></label>
+			<input type='checkbox' name='file' value='none' id="file${this.ID}" class="file" data-type="dir"/><label for="file${this.ID}">${this.name}</label>
+			<ul>
+				${this.getContents()}
+			</ul>
+		</li>`
+		}
+		return `<li><input type='checkbox' name='file' value='none' id="file${this.ID}" class="file" data-type="${this.type}"/><label for="file${this.ID}">${this.name}</label></li>`
+	};
+	
 }
 
 
-
-var me = new item("dir");
-me.addItem("dir");
-me.addItem("file");
+var me = new item("dir","grandfather");
+me.addItem("dir","father");
+me.addItem("file","son");
 console.log(me);
 
 
-$(document).ready(function(){
 
-	
-	
+
+
+
+
+$(document).ready(function () {
+	$("body").html(`<ul>${me.html()}</ul>`);
+	$("input[type=checkbox].file + label").click(fileSelect);
 });
+
+function fileSelect(e) {
+	if (!(e.originalEvent.metaKey || e.originalEvent.ctrlKey || e.originalEvent.shiftKey)) {
+		$("input[type=checkbox].file").not($(e.originalEvent.target).prev()).prop("checked", false);
+
+	} else {
+		$("#" + $(e.originalEvent.target).attr("for")).prop("checked", true);
+	}
+}
